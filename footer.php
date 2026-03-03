94553f3d9546c616a0065cb33510b2f2ffabc952
Mastering The 128 <aaron.baugher@gmail.com>
2026-03-02 19:47:54 -0600

    Create footer.php file with WordPress footer template

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+// Footer Template for WordPress
+
+?>
+<footer>
+    <div class='container'>
+        <p>&copy; <?php echo date('Y'); ?> Your Company Name. All Rights Reserved.</p>
+    </div>
+</footer>
