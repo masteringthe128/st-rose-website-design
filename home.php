f3ea8fc175d43e2736034ece8967cc79946069f8
Mastering The 128 <aaron.baugher@gmail.com>
2026-03-02 19:48:10 -0600

    Create home.php for WordPress homepage template.

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Home
+*/
+
+get_header();
+
+?>
+
+<main>
+  <h1>Welcome to Our Homepage</h1>
+  <p>This is the custom WordPress homepage template.</p>
+</main>
+
+<?php
+get_footer();
+?>
